d10692ac367e80c7e70b54d8890d0845ae4cd20c
Danilo Melo <daniloragbro@gmail.com>
2022-08-16 16:51:33 -0300

    feat(trainning) added route home

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import TextField from "../../component/TextField"
+
+function Home() {
+    return (
+        <div>
+            <h2> Home </h2>
+            <TextField />
+        </div>
+    )
+};
+
+export default Home
